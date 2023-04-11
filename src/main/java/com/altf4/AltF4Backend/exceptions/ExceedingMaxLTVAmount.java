46170e74ee99e40884bb7baaa83c813f80fc53e1
sharat789 <sharat157@gmail.com>
2023-04-11 21:01:13 +0300

    Added Exceptions

---
+++ src/main/java/com/altf4/AltF4Backend/exceptions/ExceedingMaxLTVAmount.java
@@ -0,0 +1,7 @@
+package com.altf4.AltF4Backend.exceptions;
+
+public class ExceedingMaxLTVAmount extends Exception {
+    public ExceedingMaxLTVAmount(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/altf4/AltF4Backend/exceptions/NegativeValueExceptions.java
@@ -0,0 +1,7 @@
+package com.altf4.AltF4Backend.exceptions;
+
+public class NegativeValueExceptions extends Exception{
+    public NegativeValueExceptions(String message) {
+        super(message);
+    }
+}
