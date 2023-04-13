5b444543a3d9c9ce787fe49cd509687eb9a01a6d
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-14 01:20:58 +0300

    Implement response builder and refactor

---
+++ src/main/java/com/altf4/app/model/MonthlyPaymentFormResponse.java
@@ -0,0 +1,59 @@
+package com.altf4.app.model;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class MonthlyPaymentFormResponse {
+
+    private final int monthlyPaymentAmount;
+    private final double interestRate;
+    private final int loanAmount;
+    private final int totalInterestAmount;
+    private final int totalPaymentSum;
+
+    private MonthlyPaymentFormResponse(MonthlyPaymentResponseBuilder builder) {
+        this.monthlyPaymentAmount = builder.monthlyPaymentAmount;
+        this.interestRate = builder.interestRate;
+        this.loanAmount = builder.loanAmount;
+        this.totalInterestAmount = builder.totalInterestAmount;
+        this.totalPaymentSum = builder.totalPaymentSum;
+    }
+    @Component
+    public static class MonthlyPaymentResponseBuilder{
+        private int monthlyPaymentAmount;
+        private double interestRate;
+        private int loanAmount;
+        private int totalInterestAmount;
+        private int totalPaymentSum;
+
+        public MonthlyPaymentResponseBuilder() {}
+
+        public MonthlyPaymentResponseBuilder monthlyPaymentAmount(int monthlyPaymentAmount) {
+            this.monthlyPaymentAmount = monthlyPaymentAmount;
+            return this;
+        }
+        public MonthlyPaymentResponseBuilder interestRate(double interestRate) {
+            this.interestRate = interestRate;
+            return this;
+        }
+        public MonthlyPaymentResponseBuilder loanAmount(int loanAmount) {
+            this.loanAmount = loanAmount;
+            return this;
+        }
+        public MonthlyPaymentResponseBuilder totalInterestAmount(int totalInterestAmount) {
+            this.totalInterestAmount = totalInterestAmount;
+            return this;
+        }
+        public MonthlyPaymentResponseBuilder totalPaymentSum(int totalPaymentSum) {
+            this.totalPaymentSum = totalPaymentSum;
+            return this;
+        }
+
+        public MonthlyPaymentFormResponse build() {
+            return new MonthlyPaymentFormResponse(this);
+        }
+    }
+
+}
--- src/main/java/com/altf4/app/service/LoanCalculatorService.java
+++ src/main/java/com/altf4/app/service/LoanCalculatorService.java
@@ -1,8 +1,8 @@
 package com.altf4.app.service;
 
-import com.altf4.app.model.MonthlyPaymentResponse;
-import com.altf4.app.model.MonthlyPaymentRequest;
-import com.altf4.app.validator.EstimatedMonthlyPaymentRequestValidator;
+import com.altf4.app.model.MonthlyPaymentFormResponse;
+import com.altf4.app.model.MonthlyPaymentFormRequest;
+import com.altf4.app.validator.MonthlyPaymentRequestValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,30 +12,53 @@ import static com.altf4.app.model.LoanTerms.MARGIN;
 @Service
 public class LoanCalculatorService {
 
+    private final MonthlyPaymentRequestValidator validator;
+
     @Autowired
-    private MonthlyPaymentResponse monthlyPaymentResponse;
-    @Autowired
-    private EstimatedMonthlyPaymentRequestValidator validator;
-
-
-    public MonthlyPaymentResponse calculateMonthlyAnnuityPayments(
-            MonthlyPaymentRequest monthlyPaymentRequest) {
-
-        validator.validate(monthlyPaymentRequest);
-
-        int banksLoanAmount = monthlyPaymentRequest.getTotalAmount() - monthlyPaymentRequest.getDownPayment();
-        double monthlyInterestRate = (EURIBOR_RATE + MARGIN) / 12;
-        int loanTermMonths = monthlyPaymentRequest.getTermYears() * 12;
-
-        int equatedMonthlyInstallment = (int) (Math.round(
-                banksLoanAmount * monthlyInterestRate
-                * Math.pow(1 + monthlyInterestRate, loanTermMonths)
-                / (Math.pow(1 + monthlyInterestRate, loanTermMonths) - 1) / 10.0) * 10);
-
-        monthlyPaymentResponse.setMonthlyPaymentAmount(equatedMonthlyInstallment);
-
-        return monthlyPaymentResponse;
+    public LoanCalculatorService(MonthlyPaymentRequestValidator validator) {
+        this.validator = validator;
     }
+
+
+    public MonthlyPaymentFormResponse generateMonthlyPaymentFormResponse(MonthlyPaymentFormRequest request) {
+
+        validator.validate(request);
+
+        int monthlyPayment = calculateMonthlyPayment(request);
+
+        return buildResponseObject(monthlyPayment, request);
+    }
+
+
+    private static int calculateMonthlyPayment(MonthlyPaymentFormRequest request) {
+        int banksLoanAmount = calculateLoanAmount(request);
+        double monthlyInterestRate = (EURIBOR_RATE + MARGIN) / 12;
+        int loanTermMonths = request.getTermYears() * 12;
+
+        return (int) (Math.round(banksLoanAmount * monthlyInterestRate
+                     * Math.pow(1 + monthlyInterestRate, loanTermMonths)
+                     / (Math.pow(1 + monthlyInterestRate, loanTermMonths) - 1) / 10.0) * 10);
+    }
+
+    public MonthlyPaymentFormResponse buildResponseObject(int monthlyPayment, MonthlyPaymentFormRequest request) {
+        return new MonthlyPaymentFormResponse.MonthlyPaymentResponseBuilder()
+                .monthlyPaymentAmount(monthlyPayment)
+                .interestRate(EURIBOR_RATE + MARGIN)
+                .loanAmount(calculateLoanAmount(request))
+                .totalInterestAmount(calculateTotalInterestAmount(monthlyPayment, request))
+                .totalPaymentSum(calculateLoanAmount(request) + calculateTotalInterestAmount(monthlyPayment, request))
+                .build();
+
+    }
+
+    private static int calculateLoanAmount(MonthlyPaymentFormRequest request) {
+        return request.getTotalAmount() - request.getDownPayment();
+    }
+    private static int calculateTotalInterestAmount(int monthlyPayment, MonthlyPaymentFormRequest request) {
+        return monthlyPayment * 12 * request.getTermYears() - (request.getTotalAmount() - request.getDownPayment());
+    }
+
+    
 
 
 }
