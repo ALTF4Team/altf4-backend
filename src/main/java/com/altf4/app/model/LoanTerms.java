ae2554f052ae48aaecf12458dfd5fcc8dd58766d
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-13 19:13:14 +0300

    Refactor fields into constants

--- src/main/java/com/altf4/app/model/LoanTerms.java
+++ src/main/java/com/altf4/app/model/LoanTerms.java
@@ -8,11 +8,9 @@ import org.springframework.stereotype.Component;
 public class LoanTerms {
 
     public static final double EURIBOR_RATE = 0.03335;
-
     public static final double MARGIN = 0.025;
 
-    private double loanServiceRatio;
-
-    private double loanToValueRatio;
+    public static final double LOAN_SERVICE_RATIO = 0.4;
+    public static final double LOAN_TO_DOWN_PAYMENT_RATIO = 0.85;
 
 }
--- src/main/java/com/altf4/app/validator/LoanToDownPaymentRatioException.java
+++ src/main/java/com/altf4/app/validator/LoanToDownPaymentRatioException.java
@@ -3,14 +3,14 @@ package com.altf4.app.validator;
 import com.altf4.app.exception.ValidationException;
 import com.altf4.app.model.MonthlyPaymentRequest;
 
-public class LoanToDownPaymentRatioException extends AbstractValidator<MonthlyPaymentRequest> {
+import static com.altf4.app.model.LoanTerms.LOAN_TO_DOWN_PAYMENT_RATIO;
 
-    public static final double RATIO = 0.85;
+public class LoanToDownPaymentRatioException extends AbstractValidator<MonthlyPaymentRequest> {
 
     @Override
     public void validate(MonthlyPaymentRequest request) throws ValidationException {
 
-        if ((double)(request.getLoanSize() - request.getDownPayment()) / request.getLoanSize() > RATIO) {
+        if ((double)(request.getLoanSize() - request.getDownPayment()) / request.getLoanSize() > LOAN_TO_DOWN_PAYMENT_RATIO) {
             throw new ValidationException("Requested loan's amount it too high for provided down payment." +
                     " Down payment must form at least 85% of requested loan.");
         }
