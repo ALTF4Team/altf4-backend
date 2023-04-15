19f2375ff37faee0889960a1b7e61016774c9ebd
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-15 13:58:39 +0300

    Refactor

--- src/main/java/com/altf4/app/controller/LoanCalculatorController.java
+++ src/main/java/com/altf4/app/controller/LoanCalculatorController.java
@@ -1,12 +1,11 @@
 package com.altf4.app.controller;
 
-import com.altf4.app.model.LoanCalculationResponse;
-import com.altf4.app.model.LoanCalculationRequest;
+import com.altf4.app.model.calculation.LoanCalculationRequest;
+import com.altf4.app.model.calculation.LoanCalculationResponse;
 import com.altf4.app.service.LoanCalculatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api/loan-calculator")
@@ -16,10 +15,9 @@ public class LoanCalculatorController {
     private LoanCalculatorService loanCalculatorService;
 
     @Autowired
-    public LoanCalculatorController (LoanCalculatorService loanCalculatorService) {
+    public LoanCalculatorController(LoanCalculatorService loanCalculatorService) {
         this.loanCalculatorService = loanCalculatorService;
     }
-
 
     @PostMapping
     @ResponseBody
