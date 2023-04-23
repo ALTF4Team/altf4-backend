c194afa11d3715711195ce2a67ea67bc7af04c0a
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-23 18:43:00 +0300

    Adapt LoanTermValidator to annotation

--- src/main/java/com/altf4/app/validator/LoanTermValidator.java
+++ src/main/java/com/altf4/app/validator/LoanTermValidator.java
@@ -1,13 +1,12 @@
 package com.altf4.app.validator;
 
-import com.altf4.app.exception.ValidationException;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 
-public class LoanTermValidator extends AbstractValidator<Integer> {
+public class LoanTermValidator implements ConstraintValidator<LoanTermConstraint, Integer> {
+
     @Override
-    public void validate(Integer termYears) throws ValidationException {
-
-        if (termYears > 30 || termYears < 1) {
-            throw new ValidationException("Requested loan term must be between 1 and 30 years.");
-        }
+    public boolean isValid(Integer termYears, ConstraintValidatorContext context) {
+        return termYears <= 30 && termYears >= 1;
     }
 }
