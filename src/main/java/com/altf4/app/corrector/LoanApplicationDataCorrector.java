2839aec8bba4d506be9016bf5e9157b61010e793
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-24 10:30:38 +0300

    Update data corrector

--- src/main/java/com/altf4/app/corrector/LoanApplicationDataCorrector.java
+++ src/main/java/com/altf4/app/corrector/LoanApplicationDataCorrector.java
@@ -1,7 +1,9 @@
 package com.altf4.app.corrector;
 
 import com.altf4.app.model.application.LoanApplication;
+import com.altf4.app.model.application.type.EmploymentStatus;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
@@ -11,14 +13,63 @@ import static com.altf4.app.model.application.type.ApplicationStatus.PENDING;
 @Component
 public class LoanApplicationDataCorrector {
 
-    public void correctDataInput(LoanApplication form) {
-        form.setApplicationStatus(PENDING);
-        capitalizeData(form);
+    LoanApplication loanApplication;
+
+    @Autowired
+    LoanApplicationDataCorrector(LoanApplication loanApplication) {
+        this.loanApplication = loanApplication;
     }
 
-    private void capitalizeData(LoanApplication form) {
-        capitalizeDataField(form.getCustomer(), "name");
-        capitalizeDataField(form.getCustomer(), "surname");
+    public void correctDataInput(LoanApplication loanApplication) {
+
+        loanApplication.setApplicationStatus(PENDING);
+
+        correctUserInput(loanApplication);
+    }
+
+    private void correctUserInput(LoanApplication loanApplication) {
+
+        capitalizeData(loanApplication);
+
+        clearIrrelevantFields(loanApplication);
+    }
+
+
+    private void capitalizeData(LoanApplication loanApplication) {
+        capitalizeDataField(loanApplication.getCustomer(), "name");
+        capitalizeDataField(loanApplication.getCustomer(), "surname");
+        capitalizeDataField(loanApplication.getFinancialInformation(), "industry");
+        capitalizeDataField(loanApplication.getFinancialInformation(), "position");
+    }
+
+    private void clearIrrelevantFields(LoanApplication loanApplication) {
+        EmploymentStatus employmentStatus = loanApplication.getFinancialInformation().getEmploymentStatus();
+        switch (employmentStatus) {
+            case CONTRACT_EMPLOYMENT -> clearNonContractEmploymentFields(loanApplication);
+            case SELF_EMPLOYED -> clearNonSelfEmploymentFields(loanApplication);
+            case UNEMPLOYED -> clearNonUnemployedFields(loanApplication);
+        }
+    }
+
+    private void clearNonContractEmploymentFields(LoanApplication loanApplication) {
+
+        clearSelfEmploymentFields(loanApplication);
+
+        clearUnemployedFields(loanApplication);
+    }
+
+    private void clearNonSelfEmploymentFields(LoanApplication loanApplication) {
+
+        clearContractEmploymentFields(loanApplication);
+
+        clearUnemployedFields(loanApplication);
+    }
+
+    private void clearNonUnemployedFields(LoanApplication loanApplication) {
+
+        clearContractEmploymentFields(loanApplication);
+
+        clearSelfEmploymentFields(loanApplication);
     }
 
     @SneakyThrows
@@ -28,5 +79,21 @@ public class LoanApplicationDataCorrector {
         String name = (String) field.get(obj);
         String capitalized = name.trim().substring(0, 1).toUpperCase() + name.trim().substring(1).toLowerCase();
         field.set(obj, capitalized);
+    }
+
+    private static void clearContractEmploymentFields(LoanApplication loanApplication) {
+        loanApplication.getFinancialInformation().setYearsCurrentEmployer(null);
+        loanApplication.getFinancialInformation().setCurrentEmployer(null);
+        loanApplication.getFinancialInformation().setEmploymentContractType(null);
+        loanApplication.getFinancialInformation().setIndustry(null);
+        loanApplication.getFinancialInformation().setPosition(null);
+    }
+
+    private static void clearSelfEmploymentFields(LoanApplication loanApplication) {
+        loanApplication.getFinancialInformation().setYearsSelfEmployment(null);
+    }
+
+    private static void clearUnemployedFields(LoanApplication loanApplication) {
+        loanApplication.getFinancialInformation().setSourceOfIncome(null);
     }
 }
