f6d13648d82c09be39f232fc68954c1e8edf6645
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-25 21:12:11 +0300

    Create Admin service

--- src/main/java/com/altf4/app/service/AdminService.java
+++ src/main/java/com/altf4/app/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.altf4.app.service;
 
 import com.altf4.app.model.application.LoanApplication;
+import com.altf4.app.model.application.type.ApplicationStatus;
 import com.altf4.app.repository.LoanApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,22 @@ public class AdminService {
         this.repository = repository;
     }
 
+
     public List<LoanApplication> getLoanApplications() {
         return repository.findAll();
+    }
+
+    public LoanApplication getLoanApplicationById(int id) {
+        return repository.findById(id);
+    }
+
+    public void updateApplicationStatus(int id, ApplicationStatus status) {
+        LoanApplication application = repository.findById(id);
+        application.setApplicationStatus(status);
+        repository.save(application);
+    }
+
+    public void deleteAllApplications() {
+        repository.deleteAll();
     }
 }
