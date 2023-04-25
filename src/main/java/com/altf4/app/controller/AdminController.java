473f89b1b62098c60a1734aec57e507ca1fef4ae
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-25 21:11:56 +0300

    Create Admin controller

--- src/main/java/com/altf4/app/controller/AdminController.java
+++ src/main/java/com/altf4/app/controller/AdminController.java
@@ -1,16 +1,15 @@
 package com.altf4.app.controller;
 
 import com.altf4.app.model.application.LoanApplication;
+import com.altf4.app.model.application.type.ApplicationStatus;
 import com.altf4.app.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/admin/application")
 public class AdminController {
 
     private final AdminService adminService;
@@ -20,8 +19,25 @@ public class AdminController {
         this.adminService = adminService;
     }
 
-    @GetMapping
+
+    @GetMapping()
     public List<LoanApplication> getLoanApplications() {
         return adminService.getLoanApplications();
     }
+
+    @GetMapping("/{id}")
+    public LoanApplication getLoanApplicationById(@PathVariable int id) {
+        return adminService.getLoanApplicationById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateApplicationStatus(@PathVariable int id, ApplicationStatus status) {
+        adminService.updateApplicationStatus(id, status);
+    }
+
+    @DeleteMapping
+    public void deleteAllApplications() {
+        adminService.deleteAllApplications();
+    }
+
 }
