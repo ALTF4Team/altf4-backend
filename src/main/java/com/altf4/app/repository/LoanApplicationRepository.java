21c2594c217cc4c6400fd3ac0d2a5eaa0058335c
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-25 21:13:04 +0300

    Create findById method

--- src/main/java/com/altf4/app/repository/LoanApplicationRepository.java
+++ src/main/java/com/altf4/app/repository/LoanApplicationRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Integer> {
 
+    public LoanApplication findById(int id);
+
 }
