e94e502c502749a9eaa6af8f4ec7979b28ee6d18
[evaldas] <97054786+nevaldas1@users.noreply.github.com>
2023-04-27 20:41:57 +0300

    Implement JWT, registration and authentication

---
+++ src/main/java/com/altf4/app/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.altf4.app.controller;
+
+import com.altf4.app.service.AuthenticationService;
+import com.altf4.app.model.authentication.AuthenticationRequest;
+import com.altf4.app.model.authentication.AuthenticationResponse;
+import com.altf4.app.model.authentication.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
